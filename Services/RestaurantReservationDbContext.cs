a3df538c3f153ab079ccc69f3f6f391942e07897
Haraldur <haraldur@live.dk>
2024-11-25 10:30:44 +0100

    Initial commit

---
+++ Services/RestaurantReservationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestRes.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace RestRes.Services
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; init; }
+
+
+        public DbSet<Reservation> Reservations { get; init; }
+
+
+        public RestaurantReservationDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Restaurant>();
+            modelBuilder.Entity<Reservation>();
+        }
+    }
+}
